39580cc02a3584687599afdacfff41aaa53b31bb
Арсений <Ars4112@gmail.com>
2022-04-24 23:33:49 +0300

    добавляет декоративные элементы

---
+++ source/script.js
@@ -0,0 +1,17 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-header__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navToggle.classList.contains('main-header--closed')) {
+    navToggle.classList.remove('main-header--closed');
+    navToggle.classList.add('main-header--opened');
+    navMain.classList.remove('main-nav--hidden')
+
+  } else {
+    navToggle.classList.add('main-header--closed');
+    navToggle.classList.remove('main-header--opened');
+    navMain.classList.add('main-nav--hidden')
+  }
+});
